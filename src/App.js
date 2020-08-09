8c85374154399439ee0cb2d5d6f4d59fe194d1b9
tearofenemy <calm.snow98@gmail.com>
2020-08-09 23:54:40 +0300

    implement pizza component, create selector of size and type of pizza

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import {Route} from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+
+    const [pizzas, setPizzas] = useState([]);
+ 
+    useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({data}) => {
+            setPizzas(data.pizzas);
+        })
+    }, []);
+
     return (
         <div className="wrapper">
-        <Header />
-        <div className="content">
-            <Route exact path='/' component={Home} />
-            <Route exact path='/cart' component={Cart} />
+            <Header />
+            <div className="content">
+                <Route exact path='/' render={() => <Home items={pizzas}/>} />
+                <Route exact path='/cart' component={Cart} />
+            </div>
         </div>
-    </div>
     );
 }
 
-export default App;
+export default App
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import Header from './Header';
 import Categories from './Categories';
 import SortPopup from './SortPopup';
+import Pizza from './Pizza';
 
-export { Button, Header, Categories, SortPopup };
+export { Button, Header, Categories, SortPopup, Pizza };
